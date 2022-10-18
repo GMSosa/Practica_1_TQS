ee88aea87aeee8ca4b79f21b8995899708e9c5aa
GMSosa <91845043+GMSosa@users.noreply.github.com>
2022-10-18 09:29:23 +0200

    Escritura a fichero hecha

--- node_tqs/Node_BackEnd.js
+++ node_tqs/Node_BackEnd.js
@@ -8,8 +8,21 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
-app.post('/set_score',(req, res) => {
-    res.send('Hello World!')
+
+// example http call:
+// localhost:3000/set_score?nombre=MrUwU&puntuacion=20
+app.get('/set_score',(req, res) => {
+    var name = req.query.nombre;
+    var score = req.query.puntuacion;
+    var lineScore = name + ',' + score + '\n';
+    console.log(score);
+    const fs = require('fs');
+    fs.appendFile('./scores.txt',lineScore, err => {
+        if(err){
+            console.log(err);
+        }
+    });
+    res.send('Añadido Correctamente');
 })
 
 app.listen(port, () => {

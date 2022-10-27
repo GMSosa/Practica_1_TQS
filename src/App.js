9a55088de66a4f3fb23914554e7e030651cb7317
SamuelVitales <101113991+SamuelVitales@users.noreply.github.com>
2022-10-27 12:31:27 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Card from "./components/Card";
 import { Items } from "./data/Items";
-import CounterTimer from "./components/CounterTimer";
 
 export default function App() {
   const [items, setItems] = useState(Items);
   const [prev, setPrev] = useState(-1);
   const [points, setPoints] = useState(0);
   const [winner, setWinner] = useState(false);
-  const [time, setTime] = useState({ hours: 0, minutes: 2, seconds: 0 });
+  const [time, setTime2] = useState({ hours: 0, minutes: 1, seconds: 0 });
+  const { hours, minutes, seconds } = time;
+  const [[hrs, mins, secs], setTime] = useState([hours, minutes, seconds]);
 
   function check(current) {
-    if (items[current].id === items[prev].id) {
+    if (items[current].id === items[prev].id && items[current].name !== items[prev].name) {
       items[current].state = "correct";
       items[prev].state = "correct";
       setItems([...items]);
       setPrev(-1);
       setPoints(points + 1);
-    } else {
+    } 
+    else if(items[current].id !== items[prev].id) {
       items[current].state = "wrong";
       items[prev].state = "wrong";
       setItems([...items]);
@@ -28,6 +30,13 @@ export default function App() {
         setPrev(-1);
       }, 1000);
     }
+    else if(items[current].id === items[prev].id && items[current].name === items[prev].name)
+    {
+      items[current].state = "";
+      items[prev].state = "";
+      setItems([...items]);
+      setPrev(-1);
+    }
   }
 
   function handleClick(id) {
@@ -36,9 +45,15 @@ export default function App() {
         items[id].state = "active";
         setItems([...items]);
         setPrev(id);
-      } else {
+      } 
+      else 
+      {
         check(id);
       }
+    }
+    else if (items[id].state === "correct")
+    {
+      items[id].state = "";
     }
   }
 
@@ -47,24 +62,41 @@ export default function App() {
       setWinner(true);
     }
 
-    if (winner === true) 
-    {
+    if (winner === true) {
       document.getElementById("winner").classList.add("show");
     }
-
   }, [points, winner]);
 
   const start = () => {
     setPoints(0);
     setWinner(false);
     console.log(winner);
-    document.getElementById('winner').classList.remove("show");
-    items.forEach(item => {
+    document.getElementById("winner").classList.remove("show");
+    items.forEach((item) => {
       item.state = "";
       setItems([...items]);
-    })
-    setTime({ hours: 0, minutes: 2, seconds: 0 })
-  } 
+    });
+    setTime({ hours: 0, minutes: 1, seconds: 0 });
+  };
+
+  const tick = () => {
+    if (hrs === 0 && mins === 0 && secs === 0) reset();
+    else if (mins === 0 && secs === 0) {
+      setTime([hrs - 1, 59, 59]);
+    } else if (secs === 0) {
+      setTime([hrs, mins - 1, 59]);
+    } else {
+      setTime([hrs, mins, secs - 1]);
+    }
+  };
+
+  const reset = () =>
+    setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+
+  useEffect(() => {
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
 
   return (
     <>
@@ -78,7 +110,9 @@ export default function App() {
         <div className="cards">
           <div>
             <h1> Score:{points} </h1>
-            <CounterTimer hourMinsSecs={time} />
+            <h1>{`${hrs.toString().padStart(2, "0")}:${mins
+              .toString()
+              .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`}</h1>
           </div>
           <div className="container">
             {items.map((item, index) => (
